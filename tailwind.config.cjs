d3e216d306d41373ea45e377abdd91b234865815
Oleksandr Dashko <leksdashko@gmail.com>
2023-02-10 21:34:12 -0800

    header footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,18 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       body: '#F5F5F5',
+			base: '#6E6E6E',
+			dark: '#181817',
+			pink: '#FA207A',
       'white': '#ffffff',
       'purple': '#341197',
     },
+		fontSize: {
+      sm: ['18px', '22px'],
+      base: ['22px', '24px'],
+			'base-lg': ['23px', '26px'],
+			lg: ['56px', '70px']
+    }
   },
   plugins: [
 		
